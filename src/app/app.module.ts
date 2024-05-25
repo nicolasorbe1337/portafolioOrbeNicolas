4e6bdad702de9cea14b88d0cbfb529305d558614
nicolasorbe1337 <nicolas_1337@hotmail.com>
2024-05-25 17:18:58 -0300

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProyectosComponent } from './pages/proyectos/proyectos.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
